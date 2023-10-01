736ca4536540288dcc59636214606efa4edb8761
danilopiccinni <danilopiccinni@gmail.com>
2023-10-01 22:56:33 +0200

    create draft jumbo

--- src/App.vue
+++ src/App.vue
@@ -50,9 +50,9 @@ import Footer from './components/Footer.Vue';
   }
 
   main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
     height: calc(100vh - 56px);
   }
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,15 +12,29 @@
 </script>
 
 <template>
-    <div class="container-fluid">
-        <h1 class="text-center">Home</h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, sed ipsam dolor iusto excepturi nisi nobis dicta laborum! Minima natus quam placeat repellendus iste omnis rerum error eum hic? Ex!
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error repellat tenetur laboriosam ex dolorum alias est qui. Officiis veniam, inventore officia et distinctio enim perspiciatis nisi? Fugiat vel maiores blanditiis.
-        </p>
+    <div class="">
+        <img src="../../public/img/5.jpeg" alt="asdasdaasasasdasdsa" class="w-100">
+        <div class="inner-jumbo w-50">
+            <h1 class="">Home</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, sed ipsam dolor iusto excepturi nisi nobis dicta laborum! Minima natus quam placeat repellendus iste omnis rerum error eum hic? Ex!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error repellat tenetur laboriosam ex dolorum alias est qui. Officiis veniam, inventore officia et distinctio enim perspiciatis nisi? Fugiat vel maiores blanditiis.
+            </p>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 
+    img {
+        position: relative;
+    }
+
+    .inner-jumbo {
+        position: absolute;
+        left: 0;
+        top: 50%;
+
+        background-color: rgba(0, 0, 0, 0.843);
+    }
 </style>
