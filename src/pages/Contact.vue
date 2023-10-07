3c121f01f9f56ec2318adc4a97cae433e18f4d5e
danilopiccinni <danilopiccinni@gmail.com>
2023-10-07 23:32:55 +0200

    responsive control

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -23,7 +23,7 @@ import { store } from '../store'
         <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
             a matter of hours to help you.</p>
         
-        <div class="row">
+        <div class="row flex-column align-items-center gap-5">
         
             <!--Grid column-->
             <div class="col-md-9 mb-md-0 mb-5">
@@ -71,7 +71,7 @@ import { store } from '../store'
                         <div class="col-md-12">
         
                             <div class="md-form">
-                                <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                <textarea type="text" id="message" name="message" rows="10" class="form-control md-textarea"></textarea>
                                 <label for="message">Your message</label>
                             </div>
         
@@ -82,40 +82,37 @@ import { store } from '../store'
                 </form>
         
                 <div class="text-center text-md-left">
-                    <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                    <a class="btn btn-secondary" onclick="document.getElementById('contact-form').submit();">Send</a>
                 </div>
                 <div class="status"></div>
             </div>
             <!--Grid column-->
         
             <!--Grid column-->
-            <div class="col-md-3 text-center">
-                <ul class="list-unstyled mb-0">
-                    <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                        <p>San Francisco, CA 94126, USA</p>
-                    </li>
-        
-                    <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                        <p>+ 01 234 567 89</p>
-                    </li>
-        
-                    <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                        <p>contact@mdbootstrap.com</p>
-                    </li>
-                </ul>
+            <div class="d-flex flex-column align-items-center justify-content-between">
+                <div class="d-flex flex-column align-items-center">
+                    <i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>Unterwilrain 50, 6014 Luzern, CH</p>
+                </div>
+                <div class="d-flex flex-column justifiy-content-center align-items-center">
+                    <i class="fas fa-phone fa-2x"></i>
+                    <p>+41 76 204 85 51</p>
+                </div>
+                <div class="d-flex flex-column justifiy-content-center align-items-center">
+                    <i class="fas fa-envelope fa-2x"></i>
+                    <p>danilopwebdev@gmail.com</p>
+                </div>
+
             </div>
-            <!--Grid column-->
         
         </div>
         
         </section>
-        <!--Section: Contact v.2-->
+
     </div>
 </template>
 
 <style scoped lang="scss">
 
-    section{
-        margin: 0 100px;
-    }
+
 </style>
