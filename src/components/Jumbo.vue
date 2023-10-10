b4352267455f7237ba1ff06467605022f1547975
danilopiccinni <danilopiccinni@gmail.com>
2023-10-10 19:07:36 +0200

    selfie test

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -85,7 +85,7 @@ export default {
                 </div>
 
                 <div class="cont-img col-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                    <img class="greeting z-2" src="../../public/img/selfie.png" alt="">
+                    <img class="selfie z-2" src="../../public/img/Danilo.png" alt="">
                     <div class="carousel-3d z-1">
                         <Carousel3d></Carousel3d>
                     </div>
@@ -118,6 +118,12 @@ img {
 
     .no-err {
         border: none;
+    }
+
+    .selfie {
+        border-radius: 0 0% 50% 50%;
+        rotate: -5deg;
+
     }
 
 }
