fb01cb667180b140a6dabfad0b23626921c94f10
danilopiccinni <danilopiccinni@gmail.com>
2023-10-27 22:17:16 +0200

    feat: dummy monitor to enclose the iframe

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -18,20 +18,65 @@
 </script>
 
 <template>
-    <div class="">
+
         <div class="d-flex justify-content-center">
-            <iframe :src="project.link" frameborder="0" ></iframe>
+            <div class="cont-iframe rounded">
+                <iframe class="rounded" :src="project.link" frameborder="0" ></iframe>
+            </div>
         </div>
-    </div>
+
 </template>
 
 <style scoped lang="scss">
 
-    iframe {
-        width: 90%;
-        padding-top: 150px;
-        padding-bottom: 50px;
-        aspect-ratio: 16/9;
+    .cont-iframe {
+
+        width: 90vw;
+            height: 80vh;
+
+        margin-top: 90px;
+
+        border: 1px solid black;
+
+        position: relative;
+
+        &::after {
+            content: '';
+            width: 5px;
+            height: 30px;
+            background-color:  #d4d4d4;
+            position: absolute;
+            bottom: -1px;
+            left: 50%;
+            transform: translate(-50% , 100%);
+
+            border-right: 10px ridge #d4d4d4;
+
+        }
+
+        &::before {
+            content: '';
+            width: 20vw;
+            height: 20px;
+            background-color:  #d4d4d4;
+            position: absolute;
+            bottom: -31px;
+            left: 50%;
+            transform: translate(-50% , 100%);
+            border: 5px ridge #d4d4d4;
+            border-radius: 11px 12px 0px 0px;
+            -webkit-border-radius: 11px 12px 0px 0px;
+            -moz-border-radius: 11px 12px 0px 0px;
+        }
+
+        iframe {
+            width: 100%;
+            height: 100%;
+    
+            border: 18px ridge #d4d4d4;
+    
+        }
     }
+
 
 </style>
