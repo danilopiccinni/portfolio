7636c3dd9701d2bff45cb435757b8f40369a97a6
danilopiccinni <danilopiccinni@gmail.com>
2023-10-28 23:42:35 +0200

    feat: single project page feat: monitor resizable

--- src/components/MonitorDisplay.vue
+++ src/components/MonitorDisplay.vue
@@ -6,9 +6,24 @@
         data() {
             return {
                 store,
-                project : {}
+                project : {},
+
+                // monitor-display small (false)
+                sizeDisplay : false,
             }
         },
+
+        methods : {
+            changeSizeDisplay() {
+                if (this.sizeDisplay) {
+                    this.sizeDisplay = false
+                } else {
+                    // monitor-display large (true)
+                    this.sizeDisplay = true
+                    window.scrollTo(0, 0);
+                }
+            }
+        },  
 
         mounted() {
             this.project = this.store.selectedProject
@@ -19,79 +34,138 @@
 
 <template>
 
-        <div class="d-flex justify-content-center">
-            <div class="cont-iframe rounded">
-
-                    <iframe class="rounded" :src="project.link" frameborder="0"></iframe>
-
-                    <div v-if="project.link == ''" class="message p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
-                        <span v-if="store.language == 'italian'">Ci dispiace, ma questo progetto è attualmente in fase di manutenzione</span>
-                        <span v-if="store.language == 'german'">Es tut uns leid, aber dieses Projekt befindet sich derzeit in Wartung</span>
-                        <span v-if="store.language == 'english'">We're sorry, but this project is currently undergoing maintenance</span>
-                    </div>
-
+        <div class="out d-flex align-items-center">
+            <div  :class="sizeDisplay ? '' : 'small-display'" class="cont-iframe rounded">
+                
+                <iframe class="rounded" :src="project.link" frameborder="0"></iframe>
+                
+                <div v-if="project.link == ''" class="message p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+                    <span v-if="store.language == 'italian'">Ci dispiace, ma questo progetto è attualmente in fase di manutenzione</span>
+                    <span v-if="store.language == 'german'">Es tut uns leid, aber dieses Projekt befindet sich derzeit in Wartung</span>
+                    <span v-if="store.language == 'english'">We're sorry, but this project is currently undergoing maintenance</span>
+                </div>
+                
             </div>
+            <button :class="sizeDisplay ? 'btn-success' : 'btn-danger'" class="btn button" @click="changeSizeDisplay()"><i class="fa-solid fa-power-off"></i></button>
         </div>
 
 </template>
 
 <style scoped lang="scss">
-
-    .cont-iframe {
-
-        width: 90vw;
-        height: 80vh;
-
-        margin-top: 90px;
-
-        border: 1px solid black;
-
-        position: relative;
-
-        &::after {
-            content: '';
-            width: 5px;
-            height: 30px;
-            background-color:  #d4d4d4;
-            position: absolute;
-            bottom: -1px;
-            left: 50%;
-            transform: translate(-50% , 100%);
-
-            border-right: 10px ridge #d4d4d4;
-
-        }
-
-        &::before {
-            content: '';
-            width: 20vw;
-            height: 20px;
-            background-color:  #d4d4d4;
-            position: absolute;
-            bottom: -31px;
-            left: 50%;
-            transform: translate(-50% , 100%);
-            border: 5px ridge #d4d4d4;
-            border-radius: 11px 12px 0px 0px;
-            -webkit-border-radius: 11px 12px 0px 0px;
-            -moz-border-radius: 11px 12px 0px 0px;
-        }
-            
-        iframe {
-            width: 100%;
-            height: 100%;
-    
-            border: 18px ridge #d4d4d4;
-        }
-
-        .message {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50% , -50%);
-            animation: jumper 10s linear;
-        }
-        
+.cont-iframe {
+   width: 92vw;
+   height: 80vh;
+   margin-top: 90px;
+   border: 1px solid black;
+   background-color: white;
+   position: relative;
+   transition: all 1s;
+   
+    &::after {
+        content: '';
+        width: 5px;
+        height: 30px;
+        background-color: #d4d4d4;
+        position: absolute;
+        bottom: -1px;
+        left: 50%;
+        transform: translate(-50%, 100%);
+        border-right: 10px ridge #d4d4d4;
+        transition: all 1s;
     }
 
+    &::before {
+        content: '';
+        width: 20vw;
+        height: 20px;
+        background-color: #d4d4d4;
+        position: absolute;
+        bottom: -31px;
+        left: 50%;
+        transform: translate(-50%, 100%);
+        border: 5px ridge #d4d4d4;
+        border-radius: 11px 12px 0px 0px;
+        -webkit-border-radius: 11px 12px 0px 0px;
+        -moz-border-radius: 11px 12px 0px 0px;
+        transition: all 1s;
+    }
+        
+    iframe {
+        width: 100%;
+        height: 100%;
+        border: 13px ridge #d4d4d4;
+        transition: all 4s;
+    }
+
+    .message {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        animation: jumper 10s linear;
+        transition-delay: 2s;
+    }
+}
+
+.cont-iframe.small-display {
+    width: 300px;
+    height: 200px;
+    border: 10px ridge #d4d4d4;
+    background-image: url('/img/matrix-gif.gif');
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+    & .message {
+        opacity: 0;
+        transition: 0.2s;
+    }
+
+    &::after {
+        content: '';
+        width: 5px;
+        height: 30px;
+        background-color: #d4d4d4;
+        position: absolute;
+        bottom: -1px;
+        left: 50%;
+        transform: translate(-50%, 100%);
+        border-right: 10px ridge #d4d4d4;
+        transition: all 1s;
+    }
+
+    &::before {
+        content: '';
+        width: 80px;
+        height: 10px;
+        background-color: #d4d4d4;
+        position: absolute;
+        bottom: -31px;
+        left: 50%;
+        transform: translate(-50%, 100%);
+        border: 5px ridge #d4d4d4;
+        border-radius: 11px 12px 0px 0px;
+        -webkit-border-radius: 11px 12px 0px 0px;
+        -moz-border-radius: 11px 12px 0px 0px;
+        transition: all 1s;
+    }
+
+    iframe {
+        opacity: 0;
+        transition: 1.3s;
+    }
+}
+
+.out {
+    .button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        bottom: -30px;
+        right: 5%;
+        transform: translate(-50%, -50%);
+    }
+}
 </style>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -15,18 +15,99 @@
             MonitorDisplay,
         },
 
+        created() {
+            this.project = this.store.selectedProject
+        }
+
     }
 </script>
 
 <template>
 
+    <div :class="store.checked ? 'bg-white text-dark' : 'bg-dark text-white'" class="single-project">
+        <div class="cont-project container-fluid d-flex flex-wrap">
+            <div class="ps-lg-5 ps-0 mx-auto mx-lg-0">
+                <div class="cont-monitor mb-5">
+                    <MonitorDisplay></MonitorDisplay>
+                </div>
+            </div>
 
-    <MonitorDisplay></MonitorDisplay>
+            <div class="container d-flex flex-column col-12 col-md-6 my-5 flex-grow-1">
+                
+                <!-- info -->
+                <div class="py-5">
+                    <h1>
+                        {{project.title}}
+                    </h1>
+
+                    <h4 class="type" :style="{color : project.type.color}">
+                        {{ project.type.name }}
+                    </h4>
+                    
+                    <ul class="p-0 d-flex gap-3  flex-wrap technologies">
+                        <li v-for="technology in project.technologies"  class="list-group-item">
+                            <span class="badge rounded-pill text-dark" :style="{backgroundColor : technology.color}">
+                                {{ technology.name + ' '}} 
+                            </span>
+                        </li>
+                    </ul>
+    
+                    <div class="d-flex gap-3">
+                        <div class="mb-2" >
+                            <span>Link: </span>
+                            <a :href="project.repo" class="repo">Git-Hub</a>
+                        </div>
+                        <div v-if="project.link != ''" class="mb-2" >
+                            <span>Link: </span>
+                            <a :href="project.repo" class="repo">Web-Site</a>
+                        </div>
+    
+                    </div>
+                    
+                    <p class="description">
+                        {{ project.description }}
+                    </p>
+                </div>
+
+                <!-- cover -->
+                <div class="w-100 d-flex flex-column align-items-center gap-5 mb-5" >
+                    <img :class="store.checked ? 'shadow-dark' : 'shadow-white'" class=" rounded border cont-image mt-5" :src="project.cover" alt="">
+                </div>
+                
+                <!-- v-for images  -->
+                <div class="w-100 d-flex flex-column align-items-center gap-5">
+                    <img v-for="image in project.images" :class="store.checked ? 'shadow-dark' : 'shadow-white'" class="cont-image rounded border" :src="image" alt="">
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 
 
 </template>
 
 <style scoped lang="scss">
 
+.shadow-dark{
+    box-shadow: 6px 2px 13px 6px rgba(0,0,0,0.49);
+    -webkit-box-shadow: 6px 2px 13px 6px rgba(0,0,0,0.49);
+    -moz-box-shadow: 6px 2px 13px 6px rgba(0,0,0,0.49);
+}
+
+.shadow-white {
+    box-shadow: 6px 2px 13px 6px rgba(255, 255, 255, 0.49);
+    -webkit-box-shadow: 6px 2px 13px 6px rgba(255, 255, 255, 0.49);
+    -moz-box-shadow: 6px 2px 13px 6px rgba(255, 255, 255, 0.49); 
+}
+
+.cont-monitor {
+    position: sticky;
+    top: -50px;
+}
+
+img {
+    max-width: 100%;
+}
 
 </style>
